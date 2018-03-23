0bb828d371e958d2250630c8264b1319ebd961f2
Josh Pizel <joshpizel@AD.DOE.CARLETON.CA>
2018-03-23 13:16:27 -0400

    started part 2

--- code/part2.m
+++ code/part2.m
@@ -1,5 +1,17 @@
-clear all
-
+disp('2.(a) Represents a low pass circuit with amplifier. During DC conditions, the', ...
+     'capacitor will block current however the resistors will let it', ...
+     'pass freely. The inductor will also let it pass freely.', ...
+     'However, once the current begins to alternate, the inductor', ...
+     'will begin to resist the current and the capacitor will begin',...
+     'to conduct. This will cause parralel resistance and further',...
+     'the resistance of the circuit')
+ 
+ disp('2.(b) I would expects a pass band during the low frequencies',...
+      ' and a 2nd order drop off at its respective frequency')
+  
+runTime = 1; %given in seconds
+timecuts = 1000;
+dt =runTime/timecuts;
 
 R1=1;
 C1=0.25;
@@ -33,73 +45,28 @@ G = [     1,             0,  0,    0,  0,     0,            0; ...
           0,             0,  0,    0, -a,     1,            0; ...
           0,             0,  0, 1/R3, -1,     0,            0; ...
           0,             0,  0,    0,  0, -1/R4, (1/R4 +1/Ro)];
-
+   
       
-V1 = 10;      
+%function A
+V1 = 0;      
+F = zeros(1);
 F = [V1; 0; 0; 0; 0; 0; 0];
 
-w = 0;
-V = (G+1i*w*C)\F;
-
-%V1
-%V2
-%IL
-%I3
-%V4
-%Vo
-
-for k =1:21
-    vp = -10 +k -1;
-    F(1,1) = vp;
+for count  = 1:1:timecuts
     
-    V(:,:,k) = (G+1i*w*C)\F;
-end
-
-Vo(1,:) = V(7,1,:);
-V3(1,:) = V(4,1,:).*R3;
-
-figure(1)
-plot(-10:1:10,Vo)
-title('Vo for -10 to 10 V1')
-
-figure(2)
-plot(-10:1:10,V3)
-title('V3 for -10 to 10 V1')
-
-
-%varying omega
-F(1,1) =10;
-for w = 1:1000
-    V(:,:,w) = (G+1i*w*C)\F;
-end
-
-clear Vo
-Vo(1,:) = V(7,1,:);
-
-Vo1 = 20*log10(Vo/V1);
-
-figure(3)
-semilogx(1:1000,Vo1)
-title('Gain of circuit with varying w')
-
-%Random C's
-w=pi;
-std = 0.05;
-for i = 1:100
-    Cnew = normrnd(C1,std);
-    C(2,1) = -Cnew;
-    C(2,2) = Cnew;
     
-    V(:,:,i) = (G+1i*w*C)\F;
 end
-
-clear Vo
-Vo(1,:) = V(7,1,:);
-
-Vo1 = 20*log10(Vo/V1);
-
-figure(4)
-hist(real(Vo1(:)))
-title('Gain with varying C')
-
-
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
